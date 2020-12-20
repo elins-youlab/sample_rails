4cbcf6ed0b0e628ae00938ea8bebb0c89ec11418
Tab10id <tabloidmeister@gmail.com>
2020-12-20 15:42:25 +0300

    Добавляет модель пациента
    + миграция и актуальная схема

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_123058) do
+ActiveRecord::Schema.define(version: 2020_12_18_113046) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "login"
-    t.boolean "admin"
+  create_table "doctors", force: :cascade do |t|
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "gender", null: false
+    t.integer "speciality_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["speciality_id"], name: "index_doctors_on_speciality_id"
   end
 
+  create_table "patients", force: :cascade do |t|
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "gender", null: false
+    t.date "birthday"
+    t.string "phone_number", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "specialities", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "doctors", "specialities"
 end
