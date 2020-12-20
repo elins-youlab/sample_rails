e8b8946d3749cb78accb9aa9e0ce981ffa6b5ad8
Tab10id <tabloidmeister@gmail.com>
2020-12-21 01:37:38 +0300

    Добавляет параметр пользователя admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_18_113046) do
+ActiveRecord::Schema.define(version: 2020_12_20_212227) do
 
   create_table "doctors", force: :cascade do |t|
     t.string "last_name", null: false
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2020_12_18_113046) do
     t.string "phone_number", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_patients_on_user_id"
   end
 
   create_table "specialities", force: :cascade do |t|
@@ -38,5 +40,19 @@ ActiveRecord::Schema.define(version: 2020_12_18_113046) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "doctors", "specialities"
+  add_foreign_key "patients", "users"
 end
