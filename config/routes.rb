7b6e3210c7ead25c26422c996cd3bec24548938e
Tab10id <tabloidmeister@gmail.com>
2020-12-21 01:37:20 +0300

    Добавляет интерфейс редактирования пациента

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   resources :specialities, except: :show
   resources :doctors
-  resources :patients, except: %i[new create destroy]
+  resource :patient, except: %i[new create show destroy]
 end
